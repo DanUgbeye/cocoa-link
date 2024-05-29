d3eaaacded0fff713d4be3f4bfefea19524ebe79
Daniel Ugbeye <ugbeyellionz@gmail.com>
2024-05-29 10:24:51 +0100

    fix lint issue

---
+++ src/app/auth/logout/screen.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import Spinner from "@/components/spinner";
+import { logout } from "@/server/modules/auth/auth.actions";
+import { useEffect } from "react";
+
+export default function LogoutScreen(props: { redirect?: string }) {
+  const { redirect } = props;
+
+  useEffect(() => {
+    logout(redirect);
+  }, []);
+
+  return (
+    <section>
+      <center className=" py-10 ">
+        <Spinner className=" size-10 animate-spin text-blue-600 duration-300 " />
+      </center>
+    </section>
+  );
+}
