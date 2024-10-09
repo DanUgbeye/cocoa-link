094b14518b9704cbb387a2073146633c525086c5
Daniel Ugbeye <ugbeyellionz@gmail.com>
2024-10-09 15:33:16 +0100

    populate deal image on orders page, fix type error

--- src/app/(app)/(protected-routes)/orders/page.tsx
+++ src/app/(app)/(protected-routes)/orders/page.tsx
@@ -11,7 +11,7 @@ import { PAGES } from "@/data/page-map";
 import connectDB from "@/server/db/connect";
 import { getLoggedInUser } from "@/server/modules/auth/auth.actions";
 import { OrderDocument } from "@/server/modules/order/order.types";
-import { Order, OrderWithDeal } from "@/types";
+import { Order, OrderWithDeal, OrderWithFullDeal } from "@/types";
 import { Model } from "mongoose";
 import { redirect } from "next/navigation";
 
@@ -25,6 +25,7 @@ export default async function OrdersPage() {
   const orders = await orderModel
     .find({ $or: [{ buyerId: user._id }, { sellerId: user._id }] })
     .populate("dealId")
+    .populate("dealId.image")
     .sort({ createdAt: "desc" });
 
   return (
@@ -45,7 +46,9 @@ export default async function OrdersPage() {
               <div className="py-5 text-center">no available orders</div>
             ) : (
               <OrdersTable
-                orders={JSON.parse(JSON.stringify(orders)) as OrderWithDeal[]}
+                orders={
+                  JSON.parse(JSON.stringify(orders)) as OrderWithFullDeal[]
+                }
               />
             )}
           </CardContent>
